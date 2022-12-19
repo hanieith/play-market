b5eaa52531e2a9bb39879e28f2739c02b2bda260
Hanieith <shkutkoff@gmail.com>
2022-12-19 22:12:40 +0300

    19.12

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import requests
 import json
-
+from lxml import etree
 url = "https://play.google.com/_/PlayStoreUi/data/batchexecute?rpcids=teXCtc&source-path=/store/games&f.sid=7117283802506478010&bl=boq_playuiserver_20221213.07_p3&hl=ru&gl=US&authuser=0&soc-app=121&soc-platform=1&soc-device=2&_reqid=2302788&rt=c"
-freq = '[[["teXCtc","[null,[\\"man\\"],[10],[2,1],4]",null,"generic"]]]'
+freq = '[[["teXCtc","[null,[\\"cat\\"],[10],[2,1],4]",null,"generic"]]]'
 
 
 
 response = requests.post(url, data={"f.req": freq},
-                         headers={'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-                                  "Accept" : "*/*"})
+                         headers={'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'})
 
 
-print(response.content)
+print(json.loads(response.text[10:-28])[0][2])
+
